49044ec0bed9fab2a301aa72e7bcefd2c0e9c247
guavajellyaaron <aaron@guavajellyco.com>
2018-07-11 09:47:43 -0700

    Adding YouTube API and removing keys

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import { Config } from '../config.js';
+
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const API_KEY = 'AIzaSyCBbhnbln1bjQT65_5EZtHkLKr8oROQqYo';
+console.log(Config.GOOGLE_API_KEY);
 
-// Create a new component. This component should produce some HTML
-const App = () => {
+const API_KEY = Config.GOOGLE_API_KEY;
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() { 
     return (
         <div>
             <SearchBar />
+            <VideoList videos={this.state.videos} />
         </div>
-    );
+        );
+    }
 }
 
-// Take this component's generated HTML and put it on the page (in the dom)
 ReactDOM.render(<App />, document.querySelector('.container'));
