c30b2da3a83e0a19eb12d4dba1ccb71251b45897
guavajellyaaron <aaron@guavajellyco.com>
2018-07-12 13:46:24 -0700

    Added video detail and video list pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { Config } from '../config.js';
 
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-
-console.log(Config.GOOGLE_API_KEY);
+import VideoDetail from './components/video_detail';
 
 const API_KEY = Config.GOOGLE_API_KEY;
 
@@ -15,20 +15,36 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [], 
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+        this.videoSearch('surfboards');
+        
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
     render() { 
-    return (
-        <div>
-            <SearchBar />
-            <VideoList videos={this.state.videos} />
-        </div>
-        );
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+                    videos={this.state.videos} />
+            </div>
+            );
     }
 }
 
