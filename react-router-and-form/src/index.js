4c183ba26596acc9340ca13de4d2b26631704e99
guavajellyaaron <aaron@guavajellyco.com>
2018-10-30 09:03:48 -0700

    basic react router and how to use it

--- react-router-and-form/src/index.js
+++ react-router-and-form/src/index.js
@@ -10,11 +10,22 @@ import PostIndex from './components/post_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+// show or hide depending on the path
+class Hello extends React.Component {
+  render() { return <div>Hello</div>; }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye</div>; }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Route path="/" component={PostIndex} />
+        {/* <Route path="/hello" component={Hello}></Route>
+        <Route path="/goodbye" component={Goodbye}></Route> */}
       </div>
     </BrowserRouter>
   </Provider>
