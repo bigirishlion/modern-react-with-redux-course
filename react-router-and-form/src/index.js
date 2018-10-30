f800be5a433a1909285447ab04d18a4be67cb1d8
guavajellyaaron <aaron@guavajellyco.com>
2018-10-30 10:22:51 -0700

    add fetch posts action

--- react-router-and-form/src/index.js
+++ react-router-and-form/src/index.js
@@ -3,29 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from "redux-promise";
 
 import reducers from './reducers';
 
 import PostIndex from './components/post_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-// show or hide depending on the path
-class Hello extends React.Component {
-  render() { return <div>Hello</div>; }
-}
-
-class Goodbye extends React.Component {
-  render() { return <div>Goodbye</div>; }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Route path="/" component={PostIndex} />
-        {/* <Route path="/hello" component={Hello}></Route>
-        <Route path="/goodbye" component={Goodbye}></Route> */}
       </div>
     </BrowserRouter>
   </Provider>
