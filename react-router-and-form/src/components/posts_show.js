3132e1b69e0854deaa7aaa4598fc4d7f112085a0
guavajellyaaron <aaron@guavajellyco.com>
2018-11-08 14:00:04 -0800

    add fetch post for post details page

---
+++ react-router-and-form/src/components/posts_show.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchPost } from "../actions";
+
+class PostsShow extends Component {
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+
+    render() {
+        return (
+            <div>Posts</div>
+        )
+    }
+}
+
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(null, { fetchPost })(PostsShow);
