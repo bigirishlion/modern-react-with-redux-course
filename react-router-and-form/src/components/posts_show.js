51ef5fabace78d5ab5637da2fb90fbf2fce857ff
guavajellyaaron <aaron@guavajellyco.com>
2018-11-14 10:09:00 -0800

    add delete post functionality

--- react-router-and-form/src/components/posts_show.js
+++ react-router-and-form/src/components/posts_show.js
@@ -1,18 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchPost } from "../actions";
+import { fetchPost, deletePost } from "../actions";
 
 class PostsShow extends Component {
     componentDidMount() {
+        if (!this.props.post) {
+            const { id } = this.props.match.params;
+            this.props.fetchPost(id);
+        }
+    }
+
+    onDeleteClick() {
         const { id } = this.props.match.params;
-        this.props.fetchPost(id);
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
+        const { post } = this.props
+
+        if (!post) {
+            return <div>Loading...</div>
+        }
+
         return (
-            <div>Posts</div>
-        )
+            <div>
+                <Link to="/" className="btn btn-primary">Back to Index</Link>
+                <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>Delete Post</button>
+                <h1>{post.title}</h1>
+                <h6>Categories: {post.tags}</h6>
+                <p>{post.content}</p>
+            </div>
+        );
     }
 }
 
@@ -20,4 +41,4 @@ function mapStateToProps({ posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
